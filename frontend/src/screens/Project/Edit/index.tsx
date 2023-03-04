111bb8b0133ccf39a3a9dc3785d9aab3f6e6e376
natanaeldeveloper <natanaeloliveiramartinsbr@gmail.com>
2023-03-04 15:12:46 -0300

    refactor: edit page done

--- frontend/src/screens/Project/Edit/index.tsx
+++ frontend/src/screens/Project/Edit/index.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useMemo, useState } from "react"
-import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+import { Link, useParams } from "react-router-dom"
 import api from "../../../services/api"
-import { Card, Form, Input, Row, Col, Skeleton } from 'antd'
+import { Card, Form, Input, Row, Col, Skeleton, Button, message, Space } from 'antd'
+import { SaveOutlined } from '@ant-design/icons'
 
 type ProjectPropst = {
   id: number,
@@ -9,29 +10,40 @@ type ProjectPropst = {
   description: string,
 }
 
-const { TextArea } = Input
+const ProjectEditScreen = () => {
 
-const ProjectEditScreen: React.FC = () => {
+  const { TextArea } = Input
+  const [messageApi, contextHolder] = message.useMessage()
 
   const [loading, setLoading] = useState(true);
+  const [loadingForm, setLoadingForm] = useState(false);
 
-  const stopLoader = (checked: boolean) => {
-    setLoading(!checked);
+  const stopLoading = () => {
+    setLoading(false);
+  };
+
+  const stopLoadingForm = () => {
+    setLoadingForm(false);
+  };
+
+  const startLoadingForm = () => {
+    setLoadingForm(true);
   };
 
   const [project, setProject] = useState<ProjectPropst>()
   const { projectId } = useParams()
   const [form] = Form.useForm()
+  const [fieldErrors, setFieldErrors] = useState<any>()
 
   useEffect(() => {
     api
-      .get('projects/' + projectId + '')
+      .get('projects/' + projectId)
       .then(response => {
         setProject(response.data.data)
-        stopLoader(loading)
+        stopLoading()
       })
       .catch(err => {
-        console.error(err.data?.data?.message)
+        console.error(err.data.message)
       })
 
   }, [])
@@ -43,21 +55,48 @@ const ProjectEditScreen: React.FC = () => {
     })
   }, [project])
 
+  function onFinish(values: any) {
+    startLoadingForm()
+    api
+      .put('projects/' + projectId, values)
+      .then(response => {
+        stopLoadingForm()
+        setFieldErrors({})
+        messageApi.success({
+          type: 'success',
+          content: response.data.message,
+        })
+      })
+      .catch(err => {
+        stopLoadingForm()
+        const errors: any = {}
+        for (let key in err.response.data.errors) {
+          errors[key] = {
+            validateStatus: 'error',
+            help: err.response.data.errors[key][0]
+          }
+        }
+        setFieldErrors(errors)
+      })
+  }
   return (
     <div>
+      {contextHolder}
       <Row justify="center">
-        <Col span={10}>
-          <Card>
+        <Col lg={10} sm={16} xs={20}>
+          <Card title="Formulário de edição">
             <Form
+              layout="vertical"
               name="edit"
               autoComplete="off"
               form={form}
+              onFinish={onFinish}
             >
               <Skeleton loading={loading} active>
                 <Form.Item
                   label="Nome"
                   name="name"
-                  rules={[{ required: true, message: 'Campo obrigatório' }]}
+                  {...fieldErrors?.name}
                 >
                   <Input />
                 </Form.Item>
@@ -65,10 +104,26 @@ const ProjectEditScreen: React.FC = () => {
                 <Form.Item
                   label="Descrição"
                   name="description"
-                  rules={[{ required: true, message: 'Campo obrigatório' }]}
+                  {...fieldErrors?.description}
                 >
                   <TextArea />
                 </Form.Item>
+
+                <Row justify="end">
+                  <Space>
+                    <Form.Item>
+                      <Link to="/">
+                        <Button type="text">Voltar</Button>
+                      </Link>
+                    </Form.Item>
+
+                    <Form.Item>
+                      <Button type="primary" htmlType="submit" loading={loadingForm} icon={<SaveOutlined />}>
+                        Salvar
+                      </Button>
+                    </Form.Item>
+                  </Space>
+                </Row>
               </Skeleton>
             </Form>
           </Card>
